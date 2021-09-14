c9e8654da365b7bc860f07ea844bb589cd265ea7
kayro ghost <kayrodanyell@gmail.com>
2021-09-14 11:46:38 -0300

    deploy atualizado, atualizando as urls

--- app.js
+++ app.js
@@ -19,11 +19,7 @@ app.use(cors())
 app.use('*',(req,res,next) => {
     res.header ("Access-Control-Allow-Origin","*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    if(req.method ==='OPTION'){
-        res.header("Access-Control-Allow-Methods", 'GET,POST,PUT,DELETE,PATCH');
-        return res.status(200).json({'req method' : 'option'});
-    }
-    next() 
+    
     if(req.headers['x-forwarded-proto']=='https'){
         next()
 
