bf3838f0e4f2296735310cdc06ee26d3046544ac
kayro ghost <kayrodanyell@gmail.com>
2021-09-30 10:47:47 -0300

    atualizando controller melhorenvio para automatizar token

--- controllers/melhorenvio.js
+++ controllers/melhorenvio.js
@@ -11,22 +11,39 @@ const me = new melhorEnvioSdk({
   client_id: '1434',
   client_secret: 'i7a60hMMpXuVIVFcqXgdYXJRSgZzwaLSHGr3X06S',
   sandbox: true,
-  bearer,
+  bearer:'',
   redirect_uri: 'http://localhost:5000/',
   scope:'cart-read cart-write companies-read companies-write coupons-read coupons-write notifications-read orders-read products-read products-write purchases-read shipping-calculate shipping-cancel shipping-checkout shipping-companies shipping-generate shipping-preview shipping-print shipping-share shipping-tracking ecommerce-shipping transactions-read users-read users-write webhooks-read webhooks-write'
 })  
-
+pegaToken()
 async function pegaToken() {
-  let token = apiTokens.findAll({
+  
+  let token = await apiTokens.findOne({
     where: {
         api: "menv"
         }
     })
-  console.log(token)
-  me.setToken = token.access_token 
+  await checkTokenExp(token)
+  //console.log(token)
+  //console.log(token.dataValues.expDate)
+  me.setToken = token.access_token
+
 
 }
+async function checkTokenExp(tokenObj){
 
+  let date = dayjs().format('YYYY-MM-DD')
+  let isSame = date === tokenObj.dataValues.expDate ? true : false
+  console.log(date)
+  console.log(tokenObj.dataValues.expDate)
+  console.log(isSame)
+  if (isSame) {
+    return isSame
+  }else{
+    await refreshToken()
+  }
+  
+}
 console.log('O TOKEN REINICIA CADA VEZ Q O SERVER INICIA, ENTAO ME.BEARER É TEMPORÁRIO PARA PRODUÇÃO DEVE SER UTILIZADO UM NOVO TOKEN GERADO PELAS FUNÇÕES DA CONTROLLER')
 
 
@@ -40,12 +57,8 @@ async function authenticate() {
   return url 
 }
 
-authenticate()
+//authenticate()
 
-const testeTime = dayjs().add(2592000,'seconds').format('DD/MM/YYYY')
-console.log(typeof(testeTime))
-
-//console.log(data)
 async function shipToken(code){
   try{  
     
@@ -72,20 +85,32 @@ async function shipToken(code){
 }
 
 //!TESTAR DISPARAR AS FUNÇÕES SHItOKEN E AUTHENTICATE VIA API COM SERVER RODANDO PARA GUARDAR NO BANCO, NAO HA MAIS ERROS DE TIPAGEM
-shipToken('def50200b06ea700b7935627414ce38b02dd9d2c7e61d8b423ce2e258ed05091926ed84de7b7a2e2ef5788c1817ae651469441f78227e9c94d400b56cc69bea4af9fe1d4d2f319677c03606abed782829fb6ef36635c374a77ef3e1c98a0ad03ba8723112875b0c5a4020fc5664e1340525c95fd434c85aa1f8e4cf3fa518f32b8651ecdf9d2b4fff01ae2927ebd36a7670f755fe33ad168743b5fe61e12f0b5fcd897ce024657bee2fb02f89037fa3a07d94ee6eef70c3f3dc209325d57e7805fd0af26d8c3f8ec409ebf8c5e396575965aae4ba3f791fe6b8d0481570d71629ed6e7beefa5a1dab417163fb7804967f1f9560162e9eb1c26925bb08b40815d67910404f20529a4a0a5ef80e8106e83e44b6cc231af8aff710ba6c36f21596aef6d5f19274c4160e4573da29f3f2cd073593057173829883efd7fb9339220403e381b741f5904bf2dd24f59365ae3fda5b6e1644ede6834ffa2a92fcbf7320a493a40ebc3a7cf44c1d29e5e43550306f80d75defbf26567c3626ce5f29d9cb8dd0f9276dfdd160602e34b92d0dae25afdfadec0cc2eb079db0a4c7fa5328e6f1eb86449d5db4e73e4cb638c543d3160a7bfdc8a7e29c10e0633d9b11f138dedf2b4b00cc18223ceda8c2cfba193a90c70460324cae4df8708650c31673fe04429e2963f985513217267bdcf4648acff6ffceabb1054ae61308741011338167d2655f1ee6dfa6ff8adad6e0583c8c5055f37a61184dd6bf969f11fc3cb0ecef257358ce6b72b20d198c5380c97f33c672cee388bfcca1597e525efdbfb43f0937780cf80d40e42f25241b5b5b7c835468f12ce71c8ca7043ecd313e021d143a881daed460ce7023da4f8ed951fbe5ffaf10c4140d8dd1f8898bcb7ca95836e45c69b5cda754d6c1622315021bf02ecc4d1f935676011e695a640c1fccc41ee79ecdc9d03600c676900d30f8b0036bb798d6dee068e7edd6940a4373969942d34e18433f1e422a00a3bc3c1ee3ae1de43d365c889abc5bdc8b4d90aed7b365a0827786c115942d92005fdf0c36e0dd1ca37c77c8498e0db29c9badf4a07289b6ec7a81ac7e8188db6329d59c726890e8775238e09ccc699c1a662a000fc67cdcef55ea4268ab924463f734bcdc76d20a0d20aff51b84169c2916b3e4702ac15eccb')
+//shipToken('')
 
 
 async function refreshToken(){
 
   try {
 
-    //pega o refresh toke no me.bearer e usa pra pegar  o novo token, e define o novo token em me.bearer
+    let refresh_token =  apiTokens.findOne({
+      where: {
+          api: "menv"
+          }
+      })
+
+    const tokenRefresh = await me.auth.refreshToken(refresh_token.dataValues.refreshToken)
     
-    console.log('tokenAntigo:' + me.bearer.refreshToken)
-    const tokenRefresh = await me.auth.refreshToken(me.bearer.refresh_token)
-    me.setToken = tokenRefresh
+     var newToken = {
+                    "api":"menv",
+                    "token":tokenRefresh.data.access_token,
+                    "refreshToken":tokenRefresh.data.refresh_token,
+                    "expDate": dayjs().add(tokenRefresh.data.expires_in,'seconds').format()
+     }
+    apiTokens.create(newToken)
+
+    me.setToken = tokenRefresh.data.access_token
     console.log('tokenNovo:' + me.bearer)
-    return tokenRefresh
+    return true
 
   }catch (error) {
 
@@ -100,7 +125,6 @@ async function refreshToken(){
 
 async function shipCalc(senderCEP,receiverCEP,quant){
 
-  
   console.log('me.bearer dentro de shipcalc:'+me.bearer.access_token)
   var peso = parseFloat(quant * 0.5)
     console.log('peso: '+peso)
@@ -144,6 +168,8 @@ async function shipCalc(senderCEP,receiverCEP,quant){
     
     return error
   }
+
+  
 }
 
 async function shipCart(info){
