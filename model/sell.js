7ba30f1a130505e690226efdfdef51a9c1550edf
Kayro <kayrodanyell@gmail.com>
2022-06-09 20:22:15 -0300

    subindo para o git conexao banco via driver funcionou

--- model/sell.js
+++ model/sell.js
@@ -1,63 +1,43 @@
-/* const { Sequelize,DataTypes } = require('sequelize');
 
+const { Sequelize,Model,DataTypes} = require('sequelize');
 const sequelize = new Sequelize('test', 'root', '', {
-    host: '127.0.0.1',
-    dialect: 'mysql'
-  });
-
-  async function auth(){
-
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-  //sequelize.close()
-
-  
-
-    const User = sequelize.define('User', {
-        // Model attributes are defined here
-        firstName: {
-        type: DataTypes.STRING,
-        allowNull: false
-        },
-        lastName: {
-        type: DataTypes.STRING
-        // allowNull defaults to true
-        }
-    }, {
-        // Other model options go here
-    });
-
-    
-    const products = sequelize.define('products',{
-
-        cod_livro:{type:DataTypes.INTEGER},
-        quantidade:{ type: DataTypes.INTEGER},
-        nome_autor:{type:DataTypes.STRING(50)},
-        nome_livro:{ type:DataTypes.STRING(50)},
-        descricao:{type:DataTypes.STRING(50)},
-        valor:{type: DataTypes.INTEGER}, 
-        
-    },{sequelize,modeName:'products',tableName:'products'})
-    
-    // `sequelize.define` also returns the model
-    console.log(User === sequelize.models.User);
-    console.log(sequelize.models)
-
-   let prods = await products.findAll()
-   console.log(prods)
+  host: '127.0.0.1',
+  dialect: 'mysql'
+});
+async function auth(){
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
 }
-auth();
-
-
- */
-
+}
+//auth()
 
 
 
+class TableTest extends Model {}
+
+TableTest.init({
+  // Model attributes are defined here
+  nome: {
+    type: DataTypes.STRING,
+   
+  },
+  
+},{sequelize, freezeTableName: true,timestamps: false,});
+
+// the defined model is the class itself
+console.log(TableTest === sequelize.models.TableTest); 
+
+async function findAll(){
+  var result = await TableTest.findAll()
+  console.log(result)
+}
+
+findAll()
+
+/* 
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
@@ -66,7 +46,7 @@ var con = mysql.createConnection({
   user: "root",
   port:3306,
   password: "",
-  database:"phpmyadmin"
+  database:"test"
   
 });
 
@@ -74,8 +54,9 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("connected")
   });
+  
 
-  var teste = con.query("SELECT * FROM pma_bookmark;",async function (err, result, fields) {
+  var teste = con.query("SELECT * FROM testtable;",async function (err, result, fields) {
     if (err){ return err};
 
     console.log()
@@ -84,4 +65,4 @@ con.connect(function(err) {
     return {err,result,fields}
   });
 
-  console.log(teste)
+  console.log(teste) */
